9cd2242946504162b96c74ac7a03b869fa63869c
Andrew Grigorev <ei-grad@yandex-team.ru>
2011-11-28 03:31:07 +0400

    request processing moved to separate handler class, which could be overriden for particular host/uri/methods

--- charon.py
+++ charon.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import logging
-from functools import partial
+import re
 import http.client
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 from tornado.httputil import HTTPHeaders
-
 HTTPHeaders._normalize_name = lambda x: x
 
 from tornado import httpserver, httpclient, ioloop
@@ -18,35 +19,9 @@ def httprequest_repr(self):
 
 httpclient.HTTPRequest.__repr__ = httprequest_repr
 
-logging.basicConfig(level=logging.DEBUG)
-
-fetch = httpclient.AsyncHTTPClient().fetch
-
-
-def handle_request(request):
-    logging.info("Got request: %s", request)
-    url = request.protocol + '://' + request.host
-    proxy_uri_prefix = 'http://' + request.host
-    if request.uri.lower().startswith(proxy_uri_prefix.lower()):
-        url = url + request.uri[len(proxy_uri_prefix):]
-    else:
-        url = url + request.uri
-
-    headers = HTTPHeaders()
-
-    for i in request.headers.keys():
-        if i not in ignored_client_headers:
-            headers[i] = request.headers[i]
-
-    req = httpclient.HTTPRequest(
-        url, method=request.method, headers=headers,
-        body=request.body, allow_nonstandard_methods=True,
-        follow_redirects=False
-    )
-
-    logging.debug("Sending request: %s", req)
-
-    fetch(req, callback=partial(handle_response, request))
+fetch = httpclient.AsyncHTTPClient(
+    max_clients=1000
+).fetch
 
 
 ignored_client_headers = set([
@@ -64,51 +39,115 @@ replace_headers = {
     #'Connection': 'close',
 }
 
-def handle_response(request, response):
-
-    logging.info("Got response: %s", response)
-
-    if response.code not in http.client.responses:
-        request.finish()
-        return
-
-    lines = []
-
-    lines.append("HTTP/1.1 %d %s" % (
-        response.code, http.client.responses[response.code]))
-
-    headers = HTTPHeaders()
-    for i in response.headers:
-        if i not in ignored_headers:
-            headers[i] = response.headers[i]
-
-    if len(response.body):
-        headers['Content-Length'] = str(len(response.body))
-
-    for k, v in headers.get_all():
-        if k in replace_headers:
-            v = replace_headers[k]
-        lines.append(k + ": " + v)
-
-    head = "\r\n".join(lines) + "\r\n\r\n"
-
-    if isinstance(response.body, str):
-        body = response.body
+def clean_uri(request):
+    proxy_uri_prefix = 'http://' + request.host + '/'
+    if request.uri.lower().startswith(proxy_uri_prefix.lower()):
+        uri = request.uri[len(proxy_uri_prefix):]
     else:
-        try:
-            body = response.body.decode('utf-8')
-        except:
-            try:
-                body = response.body.decode('cp1251')
-            except:
-                body = '<encoded>'
-    logging.info("Sending response:\n%s", head + body)
+        uri = request.uri
+    return uri.lstrip('/')
 
-    data = head.encode('utf-8')
-    if response.body:
-        data = data + response.body
-    request.write(data)
-    request.finish()
+
+class DefaultHandler:
+
+    def __init__(self, request):
+
+        self.request = request
+
+        url = request.protocol + '://' + request.host + '/' + clean_uri(request)
+
+        headers = HTTPHeaders()
+
+        for i in request.headers.keys():
+            if i not in ignored_client_headers:
+                headers[i] = request.headers[i]
+
+        req = httpclient.HTTPRequest(
+            url, method=request.method, headers=headers,
+            body=request.body, allow_nonstandard_methods=True,
+            follow_redirects=False,
+            request_timeout=600.,
+            connect_timeout=600.
+        )
+
+        logging.debug("Sending request: %s", req)
+
+        fetch(req, callback=self.on_fetch)
+
+    def on_fetch(self, response):
+
+        logging.debug("Got response: %s", response)
+
+        self.response = response
+
+        if response.code not in http.client.responses:
+            resp = "HTTP/1.1 %d %s" % (500, http.client.responses[500])
+            self.request.write(resp.encode('ascii'))
+        else:
+            self.request.write(self.compose_response())
+        self.request.finish()
+
+    def compose_response(self):
+
+        headers = HTTPHeaders()
+
+        for i in self.response.headers:
+            if i not in ignored_headers:
+                headers[i] = self.response.headers[i]
+
+        if self.response.body is not None:
+            headers['Content-Length'] = str(len(self.response.body))
+
+        lines = []
+
+        lines.append("HTTP/1.1 %d %s" % (
+            self.response.code,
+            http.client.responses[self.response.code]
+        ))
+
+        for k, v in headers.get_all():
+            lines.append(k + ": " + v)
+
+        head = "\r\n".join(lines) + "\r\n\r\n"
+        head = head.encode("ascii")
+
+        if isinstance(self.response.body, bytes):
+            return head + self.response.body
+        else:
+            print("Fuck!")
+            return head
+
+
+class Rule:
+
+    def __init__(self, handler, host=r'.*', uri=r'.*', methods=['GET', 'POST', 'PUT', 'DELETE']):
+        self.handler = handler
+        self.host = re.compile(host)
+        self.methods = set(methods)
+        self.uri = re.compile(uri)
+
+    def check(self, request):
+        if self.host.match(request.host) is None:
+            return False
+        if request.method not in self.methods:
+            return False
+        return self.uri.match(clean_uri(request)) is not None
+
+rules = [Rule(DefaultHandler)]
+
+try:
+    from custom_rules import custom_rules
+    rules += custom_rules
+except ImportError:
+    pass
+
+
+def handle_request(request):
+    logging.info("Got request: %s", request)
+    for rule in rules:
+        if rule.check(request):
+            rule.handler(request)
+            break
 
 
 if __name__ == "__main__":
